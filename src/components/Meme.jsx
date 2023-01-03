dfbff813e2a893c5d91fdce67de3d273d5254be4
Olivia Persson <olivia.persson@forefront.se>
2023-01-03 14:08:20 +0100

    Use state to update data in app

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,12 +3,25 @@ import memes from "../memesData";
 import "./Meme.css";
 
 const Meme = () => {
-  const [memeImg, setMemeImg] = React.useState("");
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemeImages, setallMemeImages] = React.useState(memes.data.memes);
 
   function getRandomMemeImage() {
-    const memeUrls = memes.data.memes.map((meme) => meme.url);
-    const randomMemeUrl = memeUrls[Math.floor(Math.random() * memeUrls.length)];
-    setMemeImg(randomMemeUrl);
+    const allMemeUrls = allMemeImages.map((meme) => meme.url);
+    const randomMemeUrl =
+      allMemeUrls[Math.floor(Math.random() * allMemeUrls.length)];
+
+    setMeme((prevState) => {
+      return {
+        ...prevState,
+        randomImage: randomMemeUrl,
+      };
+    });
   }
 
   return (
@@ -22,7 +35,7 @@ const Meme = () => {
           Get a new meme image
         </button>
       </div>
-      <img className="meme-image" src={memeImg} />
+      <img className="meme-image" src={meme.randomImage} />
     </div>
   );
 };
